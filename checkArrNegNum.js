ee233b01aae405cc9170ce2531039a9f6c920caa
Hemalathas25 <hemalathasm04@gmail.com>
2024-02-15 18:27:12 +0530

    Check Array for Negative Numbers

--- checkArrNegNum.js
+++ checkArrNegNum.js
@@ -14,3 +14,13 @@ Sample Input 2:
 Sample Output 2:
 Yes
 Explanation 2: The given array has negative numbers, -1 and -3. */}
+function checkNeg(n){
+    for(let i=0; i< n.length; i++){
+        if(n[i] < 0)
+           return "YES"
+    }
+    return "NO"
+}
+
+//console.log(checkNeg([11,1,13,21,3,7]))
+console.log(checkNeg([6,8,9,-1,14,8,-3,6])) 
